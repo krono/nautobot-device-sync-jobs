c8952595d6321c9c7fd401f155548c5731cca546
Tobias Pape <tobias@netshed.de>
2023-09-13 13:12:24 +0200

    add report job

--- README.md
+++ README.md
@@ -1,2 +1,39 @@
-# nautobot-device-sync-jobs
+# Nautobot Device Sync Jobs
+
 Sync Jobs for Nautobot
+
+# Missing Device Type Components
+
+#### Purpose
+
+Find devices which are missing components that are in the device type template
+
+#### Job Logic
+
+1. Enumerate all devices
+2. Get components of each device's device type
+3. Compare to already existing device components
+4. Report missing components
+
+#### Notes
+
+Uses a few tags to _exempt_ certain devices/device types/... from the sync logic
+
+----
+
+_to be implemented_
+
+# Add Device Type Components 
+
+#### Purpose 
+
+Ensure that components that are added to _device types_ are mirrored on already existing _device_ instances.
+
+#### Job Logic
+
+* *tbd* *
+
+#### Notes
+
+
+
---
+++ jobs/device_synchronization.py
@@ -0,0 +1,49 @@
+# Identify devices which are missing components from the device type definition
+from nautobot.extras.jobs import Job
+
+from nautobot.dcim.models import Device
+from nautobot.extras.models import Tag
+from nautobot.extras.utils import TaggableClassesQuery
+
+name = "Device Type Synchronization"
+
+
+
+def _no_sync_tag(name):
+  from django.utils.text import slugify
+  return Tag.objects.get_or_create(
+      name=f"↻̸{name.title()}",
+      slug=f"no-device-type-sync-{slugify(name)}",
+      description=f"Device tag to exempt devices and device types from automatic synchronization of {name}",
+      color="ffe4e1"
+      content_types=TaggableClassesQuery().as_queryset().filter(app_label='dcim')
+  )
+
+
+class MissingDeviceTypeComponents(Job):
+  class Meta:
+    name = "Missing Device Type Components"
+    description = "Find devices which are missing components that are in the device type template"
+
+  def test_find_missing(self):
+    for device in Device.objects.all():
+      dt = device.device_type
+
+      for item, templateitem, anti_tag in [
+        ('consoleports', 'consoleporttemplates', _no_sync_tag('console ports')),
+        ('consoleserverports', 'consoleserverporttemplates', _no_sync_tag('console server ports')),
+        ('powerports', 'powerporttemplates', _no_sync_tag('power ports')),
+        ('poweroutlets', 'poweroutlettemplates', _no_sync_tag('power outlets')),
+        ('interfaces', 'interfacetemplates', _no_sync_tag('interfaces')),
+        ('rearports', 'rearporttemplates', _no_sync_tag('rear ports')),
+        ('frontports', 'frontporttemplates', _no_sync_tag('front ports')),
+        ('devicebays', 'devicebaytemplates', _no_sync_tag('device bays')),
+      ]:
+        names = {i.name for i in getattr(device, item).all()}
+        templatenames = {i.name for i in getattr(dt, templateitem).all()}
+        missing = templatenames - names
+        if missing:
+          if anti_tag in [dt.tags + device.tags]:
+            self.log_info(device, f"Missing {item} {sorte(missing)!r} (exempted)")
+          else:
+            self.log_warning(device, f"Missing {item} {sorte(missing)!r}")
